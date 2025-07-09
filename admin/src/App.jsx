99eddc0061fd00f55ca71e997a1142c78da4e723
Yashvardhan Sharma <vardhanyash9352@gmail.com>
2025-07-09 17:29:50 +0530

    Sidebar imported properly

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
-import Sidebar from './components/sidebar'
+import Sidebar from './components/Sidebar'
 import { Routes, Route} from 'react-router-dom'
 import Add from './pages/Add'
 import List from './pages/List'
